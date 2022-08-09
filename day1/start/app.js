13f243bf0c8acb8e52869be37c166c23b31414e4
Aayush502 <aayushhumagain23@gmail.com>
2022-08-09 20:58:42 +0545

    feature(counter): created ui of counter

--- day1/start/app.js
+++ day1/start/app.js
@@ -1,1 +1,25 @@
-console.log("Hello world from js file");
+let value = 0;
+
+const htmlValue = document.getElementById("value");
+htmlValue.innerHTML = 0;
+
+const increaseButton = document.getElementById('increase');
+const decreaseButton = document.getElementById('decrease');
+
+increaseButton.addEventListener("click", function (event) {
+    if (value>=20){
+        alert("Value must not be more than 20")
+    }else{
+        value +=1;
+        htmlValue.innerHTML = value;
+    }
+})
+
+decreaseButton.addEventListener("click", function (event) {
+    if (value<=0){
+        alert("Value must not be less than 0")
+    }else{
+        value -=1;
+        htmlValue.innerHTML = value;
+    }
+})
