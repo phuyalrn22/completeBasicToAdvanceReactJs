4764cdc16b7a92a28ca6e3e55f87f8d6045fbbc5
Aryan Phuyal <aryan@sursatech.com>
2022-08-09 20:51:18 +0545

    fix(Counter): create counter ui

--- day1/start/app.js
+++ day1/start/app.js
@@ -1,1 +1,25 @@
-console.log("Hello world from js file");
+let value = 0;
+
+const htmlValue = document.getElementById("value");
+htmlValue.innerHTML = 0;
+
+const increaseButton = document.getElementById("increase");
+const decreaseButton = document.getElementById("decrease");
+
+increaseButton.addEventListener("click", function (event) {
+  if (value >= 20) {
+    alert("Must not be greater than 20");
+  } else {
+    value += 1;
+    htmlValue.innerHTML = value;
+  }
+});
+
+decreaseButton.addEventListener("click", function () {
+  if (value <= 0) {
+    alert("Must not be less than 0");
+  } else {
+    value -= 1;
+    htmlValue.innerHTML = value;
+  }
+});
