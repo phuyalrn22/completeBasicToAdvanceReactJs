e4edade242a7a3a0bc35e72f86705c1e245c9b56
Aayush502 <aayushhumagain23@gmail.com>
2022-08-16 12:54:51 +0545

    fix(form validation)

--- day2/app.js
+++ day2/app.js
@@ -2,16 +2,24 @@ const form = document.getElementById("register");
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   const fullName = document.getElementById("fullName").value;
+  const fullNameError = document.getElementById('fullNameError');
+  
   const userName = document.getElementById("userName").value;
+  const usernameError = document.getElementById('userNameError');
+
   const password = document.getElementById("password").value;
   const errorHtmlObject = document.getElementById("error");
   const confirmPassword = document.getElementById("confirmPassword").value;
   var error = [];
+  fullNameError.innerHTML = "";
+  usernameError.innerHTML = "";
   if (!fullName || fullName.length < 3) {
     error.push("Full Name should be valid");
+    fullNameError.innerHTML = "Full Name should be valid";
   }
   if (!userName || userName.length < 3) {
     error.push("User Name should be valid");
+    usernameError.innerHTML = "User Name should be valid";
   }
   if (!password || password.length < 3) {
     error.push("password should be valid");
@@ -27,5 +35,10 @@ form.addEventListener("submit", function (e) {
       errorString += `<li>  ${error[i]} </li>`;
     }
     errorHtmlObject.innerHTML = errorString;
+    // usernameError.innerHTML = `<li> ${uerror[0]} </li>`;
   }
 });
+
+// for(var i=1; i<=50; i=i*2){
+//     console.log(i);
+// }
