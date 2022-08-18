a8e7b6910177783ca9cd9e3459544870d501ac55
Aryan Phuyal <aryan@sursatech.com>
2022-08-18 19:56:04 +0545

    feat():add to cart feature

--- ProductPage(Ecommerce)/app.js
+++ ProductPage(Ecommerce)/app.js
@@ -1,3 +1,8 @@
+let cartCount = 0;
+const cartItem = document.getElementById("cartItem");
+cartItem.innerHTML = cartCount;
+let cartItems = [];
+
 function makeHtml(product) {
   return ` 
   <div class="col-md-4">
@@ -22,11 +27,12 @@ function makeHtml(product) {
                     </div>
                     <!-- Card info end here -->
                     <!-- Button For Card -->
-                    <div class="p-3 shoe text-center text-white mt-3-cursor">
+                    <button class="p-3 shoe text-center text-white mt-3-cursor "  onclick="
+                      changeCart(${product.id})" >
                         <span class="text-uppercase">
                             Add to cart
                         </span>
-                    </div>
+                    </button>
                 </div>
             </div>
     `;
@@ -44,6 +50,7 @@ const fetchData = () => {
       }
     })
     .then((data) => {
+      productList = data;
       const productRow = document.getElementById("productRow");
       let productsHtml = "";
       for (let i = 0; i < data.length; i++) {
@@ -56,3 +63,19 @@ const fetchData = () => {
     });
 };
 fetchData();
+
+//  cart actions
+
+const changeCart = (product) => {
+  const existCart = cartItems.includes(product);
+  // const existCart = cartItems.filter((x) => x === product);
+  // if (existCart.length > 0) {
+  if (existCart) {
+    cartItems = cartItems.filter((x) => x !== product);
+    cartCount--;
+  } else {
+    cartCount++;
+    cartItems.push(product);
+  }
+  cartItem.innerHTML = cartCount;
+};
