62d93ee430646d10de205baddeb2c72d231c545b
Rajkumaroli <olirajkumar30@gmail.com>
2022-08-23 10:01:44 +0545

    final():Final Part implement

--- react-counter-app/src/App.js
+++ react-counter-app/src/App.js
@@ -1,8 +1,8 @@
-import {useState} from 'react'
+import {useState} from 'react';
 
 const App = () =>{
-  const [value,setValue] = useState(0)
-  const [saveValues,setSaveValues] = useState([])
+  const [value,setValue] = useState(0);
+  const [saveValues,setSaveValues] = useState("");
 
   const increaseValue = ()=>{
     if(value>=20){
@@ -20,20 +20,27 @@ const App = () =>{
     }
   }
   const saveValue = ()=>{
-    setSaveValues([`,${value}`]);
+    if(saveValues.length===0){
+      setSaveValues(value.toString());
+    }
+    else if(saveValues[saveValues.length-1]==value){
+      return;
+    }else{
+      setSaveValues(saveValues + "," + value.toString());
+    }
   }
 
   return(
     <div className="container">
-    <h1 className="top">People Count App: Bus</h1><hr/><br/><br/>
-    <h2>Total Number of People: {value} </h2>
-    <div className="button">
-        <button onClick={increaseValue} className="btn">Increase People</button>
-        <button onClick={decreaseValue} className="btn">Decrease People</button>
-        <button onClick={saveValue} class="btn">Save People</button>
+      <h1 className="top">People Count App: Bus</h1><hr/><br/><br/>
+      <h2>Total Number of People: {value} </h2>
+      <div className="button">
+          <button onClick={increaseValue} className="btn">Increase People</button>
+          <button onClick={decreaseValue} className="btn">Decrease People</button>
+          <button onClick={saveValue} class="btn">Save People</button>
+      </div>
+      <h2>Available People: {saveValues} </h2>
     </div>
-    <h2>Available People: {saveValues} </h2>
-  </div>
   )
   
 }
