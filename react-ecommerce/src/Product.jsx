224a02eb54ac7588d49b0c94aa0e200bac504b17
Rajkumaroli <olirajkumar30@gmail.com>
2022-08-24 14:11:11 +0545

    productPage():To Convert Javascript Page to React Page

---
+++ react-ecommerce/src/Product.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const Product = ({product}) => {
+//    const product = props.product;
+  return (
+    // Card  Start
+    <div className="col-md-4">
+        <div className="card product mt-3">
+            <div className="product1 align-items-center p-2 text-center">
+                <img src={product.thumbnail} alt="laptop" className="rounded" width="160"/>
+                <h5>{product.name}</h5>
+                <div className="info mt3">
+                    <span className="text1 d-block">{product.description}</span>
+                </div>
+                <div className="cost mt-3 text-dark">
+                    <span>Rs.{product.price}</span>
+                    <s><span>Rs.{product.oldPrice}</span></s>
+                    <div className="star mt-3 align-items-center">
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                    </div>
+                </div>
+            </div>
+            <button className="p-3 shoe text-center text-white mt-3-cursor btn-success">
+                <span className="text-uppercase">
+                    Add to cart
+                </span>
+            </button>
+        </div>
+    </div>
+
+  );
+}
+
+export default Product;
