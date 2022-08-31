d9f2b424f7319063d28b735359aeaacfb6c0ea57
phuyalrn22 <92909608+phuyalrn22@users.noreply.github.com>
2022-08-31 19:52:21 +0545

    feat: product details page

--- ecommerce/src/Pages/productDetails.jsx
+++ ecommerce/src/Pages/productDetails.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-const ProductDetails = (props) => {
-  const query = useParams();
-  console.log(query);
-  return <div>ProductDetails</div>;
+const ProductDetails = () => {
+  const [product, setProduct] = useState({});
+  const { id } = useParams();
+  const fetchData = async () => {
+    try {
+      const res = await fetch(
+        `https://62bd8db5c5ad14c110c16cc2.mockapi.io/product/${id}`,
+        {
+          method: "get",
+        }
+      );
+      const data = await res.json();
+      setProduct(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  console.log(product);
+  return <div>{product.name}</div>;
 };
 
 export default ProductDetails;
