679e0285b4a60b559e426cd4eab4b73952e82587
phuyalrn22 <92909608+phuyalrn22@users.noreply.github.com>
2022-09-05 20:26:26 +0545

    fix(Typescript): enum type and function

--- typescript/dist/index.js
+++ typescript/dist/index.js
@@ -1,23 +1,5 @@
 "use strict";
-const person = {
-    name: "Aryan Phuyal",
-    age: 24,
-    color: 1235,
+const sum = (a = 12, b = 14) => {
+    return a + b;
 };
-const person2 = {
-    name: "Aryan Phuyal",
-    age: 12,
-    color: "black",
-    profession: "It",
-    salary: 123,
-};
-const test = (b) => {
-    if (typeof b === "string") {
-        return b.length;
-    }
-    else {
-        return b.toString().length;
-    }
-};
-console.log(test(12));
-console.log(test("123"));
+console.log(sum(12, 10));
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -27,39 +27,84 @@
 
 // console.log(ShirtSize.Paid);
 
-type Person = {
-  name: string;
-  age: number;
-  color: string | number;
-};
+// type Person = {
+//   name: string;
+//   age: number;
+//   color: string | number;
+// };
 
-const person: Person = {
-  name: "Aryan Phuyal",
-  age: 24,
-  color: 1235,
-};
+// const person: Person = {
+//   name: "Aryan Phuyal",
+//   age: 24,
+//   color: 1235,
+// };
 
-type Profession = {
-  profession: string;
-  salary: number;
-};
+// type Profession = {
+//   profession: string;
+//   salary: number;
+// };
 
-type PersonDetails = Person & Profession;
+// type PersonDetails = Person & Profession;
 
-const person2: PersonDetails = {
-  name: "Aryan Phuyal",
-  age: 12,
-  color: "black",
-  profession: "It",
-  salary: 123,
-};
+// const person2: PersonDetails = {
+//   name: "Aryan Phuyal",
+//   age: 12,
+//   color: "black",
+//   profession: "It",
+//   salary: 123,
+// };
 
-const test = (b: string | number) => {
-  if (typeof b === "string") {
-    return b.length;
-  } else {
-    return b.toString().length;
-  }
-};
-console.log(test(12));
-console.log(test("123"));
+// // const test = (b: string | number) => {
+// //   if (typeof b === "string") {
+// //     return b.length;
+// //   } else {
+// //     return b.toString().length;
+// //   }
+// // };
+// // console.log(test(12));
+// // console.log(test("123"));
+// console.log(person);
+// console.log(person2);
+
+// enum Gender {
+//   Male = 0,
+//   Female = 1,
+// }
+
+// type Person = {
+//   name: string;
+//   age: number;
+//   gender: Gender;
+// };
+
+// const person: Person = {
+//   name: "Aryan",
+//   age: 24,
+//   gender: Gender.Male,
+// };
+// console.log(person);
+
+// tuple
+
+// const [counter,setCounter] = useState()
+// var a: [string, number,string,string,number];
+// a = ["asdas", 12];
+
+// const sum = (a: number, b: number) => {
+//   return a + b;
+// };
+
+// console.log(sum(12, 10));
+
+// let cb: () => number;
+
+// // let addToCart: (productId: number) => void;
+
+// const calculate = (a: number, b: number, cb: (a: string) => number) => {
+//   let sum = a + b;
+//   cb(sum.toString());
+// };
+
+// calculate(12, 15, (a) => {
+//   return "12";
+// });
